f7d4fb7cc8a96f1c8f350e1f8035a0beca94f0b2
Dahunsi John <dahunsijohn@gmail.com>
2021-02-16 23:31:55 +0100

    menu-items navigation withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepages/homepage.component';
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Switch>
+//         <Route exact path='/' component={HomePage} />
+//       </Switch>
+//     </div>
+//   );
+// }
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepages/homepage.component.jsx
+++ src/pages/homepages/homepage.component.jsx
@@ -4,7 +4,7 @@ import Directory from '../../components/menu-item/directory/directory.component'
 import './homepage.styles.scss';
 
 
-const HomePage = () => 
+const HomePage = ( ) => 
 
 (
  <Directory />
